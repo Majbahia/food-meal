667be11178907e6970519c9e52f845a3c0b308a8
Majbahia <majbahia@gmail.com>
2022-12-23 01:38:15 +0600

    display title, imag, and description

--- js/app.js
+++ js/app.js
@@ -7,5 +7,25 @@ const searchFood = () => {
     // console.log(url);
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => displaySearchResult(data.meals));
+}
+const displaySearchResult = meals =>{
+    const searchResult = document.getElementById('searchResult');
+    meals.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML=`
+        <div class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        `
+        searchResult.appendChild(div);
+    });
+    
+    
 }
